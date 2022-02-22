88f2a286b218c9cdd0bea11e8b73a2b699fcb2b0
jungseokman <seokman.jung@gmail.com>
2022-02-22 20:21:39 +0900

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "seokman", age = 32, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,12 @@
+const name = "seokman",
+    age = 32,
+    gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+};
+
+sayHi(name, age);
+
+
+export {};
